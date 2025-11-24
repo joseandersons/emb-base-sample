8cc9f4b2c62dc0d9309e1ee20fae0cb88b91f0c6
joseandersons <joseandersonsilv4@gmail.com>
2025-11-23 23:26:33 -0300

    add api display

---
+++ include/display.h
@@ -0,0 +1,6 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void display_update(int velocidade, const char *tipo, int limite);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,23 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
+#include "display.h"
 
-
-int main(void)
+void main(void)
 {
-    return 0;
+    while (1) {
+
+        display_update(55, "Leve", 60);
+        k_sleep(K_SECONDS(10000));
+
+        display_update(68, "Leve", 60);
+        k_sleep(K_SECONDS(10000));
+
+        display_update(92, "Leve", 60);
+        k_sleep(K_SECONDS(10000));
+
+        display_update(45, "Pesado", 40);
+        k_sleep(K_SECONDS(10000));
+
+        display_update(78, "Pesado", 40);
+        k_sleep(K_SECONDS(10000));
+    }
 }
