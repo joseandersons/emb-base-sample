f6872c9ed5fcc53d043da749ce677772286a3e6f
Higor Lima <limahigor.g@gmail.com>
2025-11-24 23:17:35 -0300

    feat: add complete radar logic, including axis counting and speed calculation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include "zephyr/toolchain.h"
+#include <signal.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/gpio/gpio_emul.h>
@@ -10,6 +13,40 @@
 
 LOG_MODULE_REGISTER(semaforo);
 
+enum STATE {
+	IDLE = 0,
+	READING,
+	DONE
+};
+
+enum TYPE {
+	LEVE = 0,
+	PESADO
+};
+
+enum FROM {
+	SENSOR_0 = 0,
+	SENSOR_1,
+	TIMER
+};
+
+struct data {
+	union item {
+		enum FROM from;
+		enum TYPE type;
+	} item;
+
+	union value {
+		double speed;
+		uint32_t timestamp;
+	} value;
+
+	int eixos;
+};
+
+K_MSGQ_DEFINE(sensor_queue, sizeof(struct data), 10, 4);
+K_MSGQ_DEFINE(vehicle_queue, sizeof(struct data), 10, 4);
+
 static const struct device *const console_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 static const struct gpio_dt_spec sensor0 = GPIO_DT_SPEC_GET(DT_ALIAS(button0), gpios);
@@ -18,18 +55,113 @@ static const struct gpio_dt_spec sensor1 = GPIO_DT_SPEC_GET(DT_ALIAS(button1), g
 static struct gpio_callback sensor0_data;
 static struct gpio_callback sensor1_data;
 
+static enum STATE radar_state = IDLE;
+
 static void sensor0_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	static int count = 0;
+	struct data data;
 
-	LOG_INF("Test cb 1: %d\n", ++count);
+	data.item.from = SENSOR_0;
+	data.value.timestamp = k_uptime_get_32();
+
+	k_msgq_put(&sensor_queue, &data, K_NO_WAIT);
 }
 
 static void sensor1_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	static int count = 0;
+	struct data data;
 
-	LOG_INF("Test cb 2: %d\n", ++count);
+	data.item.from = SENSOR_1;
+	data.value.timestamp = k_uptime_get_32();
+
+	k_msgq_put(&sensor_queue, &data, K_NO_WAIT);
+}
+
+void timeout_vehicle_handler(struct k_timer *dummy)
+{
+	ARG_UNUSED(dummy);
+	struct data vehicle_stop;
+
+	vehicle_stop.item.from = TIMER;
+
+	k_msgq_put(&sensor_queue, &vehicle_stop, K_NO_WAIT);
+}
+
+K_TIMER_DEFINE(timeout_vehicle, timeout_vehicle_handler, NULL);
+
+void sensor_thread(void *arg1, void *arg2, void *arg3)
+{
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+
+	struct data vehicle;
+	struct data recv_data;
+
+	bool speed_done = false;
+	int count_sensor_0 = 0;
+	int init_timestamp = 0;
+	int err;
+
+	while (true) {
+
+		if (radar_state != DONE) {
+			err = k_msgq_get(&sensor_queue, &recv_data, K_MSEC(20));
+			if (err) {
+				continue;
+			}
+		}
+
+		switch (radar_state) {
+		case IDLE:
+			if (recv_data.item.from == SENSOR_0) {
+				count_sensor_0 = 1;
+				init_timestamp = recv_data.value.timestamp;
+				radar_state = READING;
+
+				k_timer_start(&timeout_vehicle, K_MSEC(700), K_NO_WAIT);
+			}
+
+			break;
+		case READING:
+			if (recv_data.item.from == SENSOR_0) {
+				count_sensor_0++;
+				k_timer_stop(&timeout_vehicle);
+				k_timer_start(&timeout_vehicle, K_MSEC(700), K_NO_WAIT);
+			} else if (recv_data.item.from == SENSOR_1) {
+				uint32_t dt = recv_data.value.timestamp - init_timestamp;
+				vehicle.value.speed = (10000.0 / dt);
+				speed_done = true;
+			} else if (recv_data.item.from == TIMER) {
+				radar_state = DONE;
+			}
+
+			break;
+		case DONE:
+			if (!speed_done) {
+				goto end;
+			}
+
+			if (count_sensor_0 == 2) {
+				vehicle.item.type = LEVE;
+			} else if (count_sensor_0 > 2) {
+				vehicle.item.type = PESADO;
+			} else {
+				goto end;
+			}
+
+			vehicle.eixos = count_sensor_0;
+			k_msgq_put(&vehicle_queue, &vehicle, K_NO_WAIT);
+
+		end:
+			radar_state = IDLE;
+			count_sensor_0 = 0;
+			speed_done = false;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 void control_thread(void *arg1, void *arg2, void *arg3)
@@ -38,13 +170,31 @@ void control_thread(void *arg1, void *arg2, void *arg3)
 	ARG_UNUSED(arg2);
 	ARG_UNUSED(arg3);
 
-	printk("Test\n");
+	struct data vehicle;
+	int err;
+
+	while (true) {
+		err = k_msgq_get(&vehicle_queue, &vehicle, K_FOREVER);
+		if (err) {
+			continue;
+		}
+
+		if (vehicle.value.speed > CONFIG_RADAR_SPEED_LIMIT_LIGHT_KMH &&
+		    vehicle.item.type == LEVE) {
+			LOG_INF("Infracao, veiculo LEVE: %d km/h", (int)(vehicle.value.speed));
+		} else if (vehicle.value.speed > CONFIG_RADAR_SPEED_LIMIT_HEAVY_KMH &&
+			   vehicle.item.type == PESADO) {
+			LOG_INF("Infracao, veiculo PESADO: %d km/h", (int)(vehicle.value.speed));
+		}
+	}
 }
 
 int main(void)
 {
 	int ret;
 	uint8_t c;
+
+	LOG_INF("Starting system...");
 
 	if (!gpio_is_ready_dt(&sensor0) || !gpio_is_ready_dt(&sensor1)) {
 		return 0;
@@ -100,4 +250,5 @@ int main(void)
 	return 0;
 }
 
-K_THREAD_DEFINE(my_tid, 1024, control_thread, NULL, NULL, NULL, 4, 0, 0);
+K_THREAD_DEFINE(control_thread_id, 1024, control_thread, NULL, NULL, NULL, 4, 0, 0);
+K_THREAD_DEFINE(sensor_thread_id, 1024, sensor_thread, NULL, NULL, NULL, 4, 0, 0);
